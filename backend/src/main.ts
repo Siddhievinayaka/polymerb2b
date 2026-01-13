5feec4196144e43e3aa13513d1afd0625e3a0071
Siddhievinayaka <siddhievinayakakharaat5@gmail.com>
2026-01-13 17:29:18 +0530

    main.ts changed commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,26 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
-  
+  const app = await NestFactory.create(AppModule)
+
+  app.enableCors()
+  app.useGlobalPipes(new ValidationPipe())
+
   const config = new DocumentBuilder()
     .setTitle('Polymer Trading API')
     .setDescription('B2B Polymer Trading Platform API')
     .setVersion('1.0')
     .addBearerAuth()
-    .build();
-  
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  
-  await app.listen(3002);
-  console.log('Backend running on http://localhost:3002');
-  console.log('API docs available at http://localhost:3002/api');
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
+  const port = process.env.PORT || 3002
+  await app.listen(port, '0.0.0.0')
+
+  console.log(`Backend running on port ${port}`)
 }
-bootstrap();
+bootstrap()
